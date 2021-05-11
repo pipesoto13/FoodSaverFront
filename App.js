e377d2d4b04cbad37c4932a73a223b85f7109836
pipesoto13 <51277503+pipesoto13@users.noreply.github.com>
2021-05-10 19:49:09 -0500

    Add tab navigation
    * Create seller register component
    
    * Add tab navigation
    
    * Fix behavior

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SellerRegisterForm from "./components/SellerRegisterForm";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Form" component={SellerRegisterForm}/>
+        <Tab.Screen name="Home" component={SellerRegisterForm}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
