d3548856cf1f28c8a5625ae322ca9b016471dfd3
pipesoto13 <51277503+pipesoto13@users.noreply.github.com>
2021-05-12 04:28:49 -0500

    Create home screen

---
+++ screens/Home.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+  Button,
+} from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import axios from 'axios'
+
+export default function Posts() {
+  const navigation = useNavigation()
+  const [posts, setPosts] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    axios({
+      method: 'GET',
+      baseURL: 'http://192.168.0.11:8000',
+      url: '/products'
+    })
+      .then(({ data }) => {setPosts(data)})
+      .catch((err) => {setError(true)})
+      .finally(() => setLoading(false))
+  }, [])
+
+  if(loading) return <ActivityIndicator />
+  if(error) return <Text>Algo salió mal</Text>
+  return (
+    <View>
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => (
+          <View>
+            <Text style={styles.title}>{item.name}</Text>
+            <Text style={styles.title}>{item.price}</Text>
+            <Text style={styles.body}>{item.description}</Text>
+            <Button
+              title="View more"
+              onPress={() => navigation.navigate('Post', { id: item.id })}
+            />
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: '800',
+  },
+  body: {
+    fontSize: 16,
+  }
+})
