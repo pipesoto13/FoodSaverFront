bbf2dfa3ae83c42aa7b8e9fbd8544f764d6079f3
Felipe Soto Correa <pipesoto13@gmail.com>
2021-05-25 16:29:43 -0500

    Add logo to splash screen

--- screens/Splash.js
+++ screens/Splash.js
@@ -60,7 +60,7 @@ const SplashScreen = ({navigation}) => {
 export default SplashScreen;
 
 const {height} = Dimensions.get("screen");
-const height_logo = height * 0.28;
+const height_logo = height * 0.40;
 
 const styles = StyleSheet.create({
   container: {
